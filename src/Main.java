3fb7f833b774f314c4f479ec13e282838d1e754f
Nastia Mikh <nastiamikh@mail.ru>
2020-05-22 01:27:54 +0800

    Test added

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,14 @@ public class Main {
     public static void main (String[] args) {
 
        Calculate test = new Calculate();
-       long payment = test.calculate(1_000_000, 36, 9.99);
 
-       System.out.println(payment);
+       long payment12month = test.calculate(1_000_000, 12, 9.99);
+       System.out.println(payment12month);
+
+       long payment24month = test.calculate(1_000_000, 24, 9.99);
+        System.out.println(payment24month);
+
+       long payment36month = test.calculate(1_000_000, 36, 9.99);
+        System.out.println(payment36month);
     }
 }
